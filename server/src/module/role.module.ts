fa673876db4dd8deb48c01d1f6a0c9ad9457637e
Dũng Nguyễn Đình <dungnd8594@gmail.com>
2021-04-07 02:16:20 +0700

    init

--- server/src/module/role.module.ts
+++ server/src/module/role.module.ts
@@ -2,7 +2,6 @@ import { DynamicModule, Global, Module, Provider } from "@nestjs/common";
 import { Adapter, Enforcer } from "casbin";
 import { ConnectionOptions } from "typeorm";
 import TypeORMAdapter from "typeorm-adapter";
-import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOptions";
 import { RoleService } from "../service/role.service";
 import { ROLE_ENFORCER } from "../service/utils/role.constants";
 
@@ -10,7 +9,7 @@ import { ROLE_ENFORCER } from "../service/utils/role.constants";
 @Module({})
 export class RoleModule {
   public static forRootAsync(
-    dbConnectionOptions: MysqlConnectionOptions,
+    dbConnectionOptions: ConnectionOptions,
     casbinModelPath: string
   ): DynamicModule {
     const casbinEnforcerProvider: Provider = {
